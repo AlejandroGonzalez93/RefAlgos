1b5ca47289a5089f9ef67dd2c9ee8dff4e5fdc29
Alejandro González <smaedos@hotmail.com>
2025-02-23 09:28:35 +0100

    Added JAVA "Add two numbers" algorithm

--- Java/src/main/java/AddTwoNumbers.java
+++ Java/src/main/java/AddTwoNumbers.java
@@ -1,9 +1,58 @@
+import java.util.Objects;
 
-public class AddTwoNumbers {
+/* https://leetcode.com/problems/add-two-numbers/submissions/1552618467/ */
 
+public class AddTwoNumbers {	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ListNode l1 = ListNode.createListNode(new int[] {9,9,9,9,9,9,9});
+		ListNode l2 = ListNode.createListNode(new int[] {9,9,9,9});
+		
+		ListNode.printListNode(addTwoNumbersRecursive(l1, l2, false));
 	}
+	
+	/* Recursive method for adding the listnodes. */
+	static ListNode addTwoNumbersRecursive(ListNode l1,ListNode l2, boolean carry) {
+        if ( Objects.isNull(l1) && Objects.isNull(l2) ) {
+            if (carry) return new ListNode(1);
+        	return null;
+        }
 
+        int accumulated = 0;
+        if (!Objects.isNull(l1)) accumulated += l1.val; l1 = l1.next;
+        if (!Objects.isNull(l2)) accumulated += l2.val; l2 = l2.next;        
+        if (carry) accumulated++;
+        
+        carry = false;
+        if (accumulated >= 10) carry = true; accumulated -= 10;
+
+        return new ListNode(accumulated, addTwoNumbersRecursive(l1,l2,carry));	
+	}
+}
+
+class ListNode {
+	public int val;
+	public ListNode next;
+	ListNode() {}
+	ListNode(int val) { this.val = val; }
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }	
+	
+	static ListNode createListNode(int[] intList) {
+		if (intList.length==0) return new ListNode();
+		return createListNodeRecursive(intList[0], intList, 0);
+	}
+	
+	private static ListNode createListNodeRecursive(int val, int[] nums, int position) {
+		if (position == nums.length-1) { 
+			return new ListNode(nums[position]); 
+		}
+		position++;
+		return new ListNode(val, createListNodeRecursive(nums[position], nums, position));
+	}
+	
+	static void printListNode(ListNode listNode) {
+		while ( !(listNode == null || Integer.valueOf(listNode.val).equals(null)) ) {
+			System.out.println(listNode.val);
+			listNode = listNode.next;
+		}
+	}
 }
